7e5eef47a158a239d2aad9c504572f517b62e7dc
Punit Diwan <punitdiwan@gmail.com>
2019-08-26 17:28:44 +0530

    Changed About Page for Zeit Now Summery Plougin

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 const About = () => (
   <Layout>
     <div>
-      <h1>About BitzPrice</h1>
+      <h1>Changing About Page with testing Summery Plugin for Now.sh</h1>
       <p>Application to view Bitcoin prices</p>
     </div>
   </Layout>
