990015986027b44d55f9ff534134a2bf092b9897
Punit Diwan <punitdiwan@gmail.com>
2019-08-26 17:07:47 +0530

    Change some Line to Test Ineggration

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Prices from "../components/Prices";
 const Index = props => (
   <Layout>
     <div>
-      <h1>Welcome to BitzPrice Git Integration</h1>
+      <h1>Welcome to Github Integration Testing.</h1>
       <p>Check current Bitcoin rate</p>
       <Prices bpi={props.bpi} />
     </div>
