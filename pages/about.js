88e4d0e430e997b35ae99cf18013d6676ca74c2e
Punit Diwan <punitdiwan@gmail.com>
2019-08-30 16:21:38 +0530

    Changed About Page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,10 @@ import Layout from "../components/Layout";
 const About = () => (
   <Layout>
     <div>
-      <h1>Changing About Page with testing Summery Plugin for Now.sh</h1>
+      <h1>
+        {" "}
+        Vineet Diwan Changing About Page with testing Summery Plugin for Now.sh
+      </h1>
       <p>Again Changing the About Page to Test again</p>
     </div>
   </Layout>
