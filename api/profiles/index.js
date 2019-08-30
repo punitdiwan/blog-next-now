419ffb93b111156c5f415951c74c82f7a41c284f
Punit Diwan <punitdiwan@gmail.com>
2019-08-30 17:10:41 +0530

    Creating Table

--- api/profiles/index.js
+++ api/profiles/index.js
@@ -2,6 +2,14 @@ const db = require("../../lib/db");
 const escape = require("sql-template-strings");
 
 module.exports = async (req, res) => {
+  const profiles = await db.query(escape`
+  CREATE TABLE records (
+    id int(11) DEFAULT NULL,
+    name varchar(20) DEFAULT NULL,
+    address varchar(50) DEFAULT NULL,
+    avtar varchar(100) DEFAULT NULL,
+    email varchar(50) DEFAULT NULL
+  )`);
   let page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 9;
   if (page < 1) page = 1;
