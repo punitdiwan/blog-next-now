a5557e99d22686ccd63f06ea0b65ac201e2eb5c4
Punit Diwan <punitdiwan@gmail.com>
2019-08-31 15:26:06 +0530

    Removed NextJS Code kept only db code

--- api/profiles/index.js
+++ api/profiles/index.js
@@ -16,8 +16,22 @@ module.exports = async (req, res) => {
     updated_at timestamp NULL DEFAULT NULL,
     PRIMARY KEY (id)
   );
-
+  CREATE TABLE additional_exam_groups (
+    id int(11) NOT NULL,
+    name varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+    batch_id int(11) DEFAULT NULL,
+    exam_type varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+    is_published tinyint(1) DEFAULT '0',
+    result_published tinyint(1) DEFAULT '0',
+    students_list varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+    exam_date date DEFAULT NULL,
+    created_at timestamp NULL DEFAULT NULL,
+    updated_at timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (id),
+    KEY idx_additional_exam_groups_on_batch_id (batch_id),
+    CONSTRAINT fk_batch_id_on_batches_id FOREIGN KEY (batch_id) REFERENCES batches (id)
+  )
   `);
 
-  res.status(200).json({ record });
+  res.status(200).json({ Message: "record" });
 };
