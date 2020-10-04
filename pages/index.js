dd4c682fe0386896220e72daf98e84098e812866
Punit Diwan <punitdiwan@gmail.com>
2020-10-04 12:17:28 +0530

    Sample Next.js App

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,52 @@
-import Fetch from "isomorphic-unfetch";
-import Layout from "../components/Layout";
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
-import Prices from "../components/Prices";
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello, I’m <strong>Shu</strong>. I’m a software engineer and a
+          translator (English/Japanese). You can contact me on{' '}
+          <a href="https://twitter.com/chibicode">Twitter</a>.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
 
-const Index = props => (
-  <Layout>
-    <div>
-      <h1>Welcome to Github Integration Testing.</h1>
-      <p>Check current Bitcoin rate</p>
-      <Prices bpi={props.bpi} />
-    </div>
-  </Layout>
-);
-
-Index.getInitialProps = async function() {
-  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
-  const data = await res.json();
-
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
   return {
-    bpi: data.bpi
-  };
-};
-
-export default Index;
+    props: {
+      allPostsData
+    }
+  }
+}
